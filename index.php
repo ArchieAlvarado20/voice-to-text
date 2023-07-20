0b739e555d8fa7fe9afa0a52e12e2d5eb69aab4d
unknown <nive54@gmail.com>
2023-07-20 17:07:27 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Voice to Text</title>
+</head>
+<body>
+     <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card mt-4">
+                    <div class="card-header text-center">
+                            <h4>Voice to text</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-12 ">
+                              
+                                <div class="input-group mb-3 align-items-center">
+                                    <textarea  id="convert_text" autocomplete="off" class="form-control"></textarea>
+                                    <button class="btn btn-primary" id="click_to_convert">Search</button>
+                                </div>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    <script src="script.js">
+
+       
+    </script>
+</body>
+</html>
